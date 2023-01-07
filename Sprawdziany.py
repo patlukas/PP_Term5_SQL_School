764893d4e564be1513419de72067537d7f5eb186
patlukas <lukaszewski.patryk06@gmail.com>
2023-01-07 19:35:24 +0100

    Change Sprawdziany: code refactoring

--- Sprawdziany.py
+++ Sprawdziany.py
@@ -9,30 +9,23 @@ class Sprawdziany(Methods):
         self.__window = window
         self.__db = db
         self.__list_labels = ["Klasa", "Przedmiot (Nauczyciel)", "Data", "Opis"]
-        self.__list_przedmiot_and_teacher = self.__get_list_przedmiot_and_teacher()
-        self.__list_klasy = self.__get_list_klasy()
+        self.__list_przedmiot_and_teacher = []
+        self.__list_klasy = []
         self.__list_id = []
         self.__rows = []
 
     def show_frame(self) -> None:
-        self.__db.commit()
         self.__list_przedmiot_and_teacher = self.__get_list_przedmiot_and_teacher()
         self.__list_klasy = self.__get_list_klasy()
         self.__rows, self.__list_id = self.__get_rows_data()
-        for x in self.__window.winfo_children():
-            x.destroy()
-        frame = tk.Frame(master=self.__window)
-        tk.Label(master=frame, text="Zajęcia").pack()
-        self._create_table(frame, self.__list_labels, self.__rows, self.__frame_edit, self.__frame_del_row).pack()
-        tk.Button(master=frame, text="Dodaj nowe zajęcia", command=self.__frame_add).pack()
-        frame.pack()
+        self._create_main_frame(self.__db, self.__window, "Sprawdziany", "Dodaj nowy sprawdzian", self.__list_labels,
+                                self.__rows, self.__frame_add, self.__frame_edit, self.__frame_del).pack()
 
     def __get_rows_data(self):
         cur = self.__db.cursor()
         cur.execute("SELECT * FROM sprawdziany")
-        rows_read = cur.fetchall()
         rows, list_id = [], []
-        for row in rows_read:
+        for row in cur.fetchall():
             przedmiot_and_teacher = ""
             for przedmiot_name, teacher_pesel, teacher_name, full_name in self.__get_list_przedmiot_and_teacher():
                 if row[5] == teacher_pesel and row[4] == przedmiot_name:
@@ -50,7 +43,8 @@ class Sprawdziany(Methods):
 
     def __get_list_przedmiot_and_teacher(self):
         cur = self.__db.cursor()
-        cur.execute("SELECT np.przedmiot_nazwa, p.pesel, p.nazwisko || ' ' || p.imie FROM nauczyciele_przedmioty np JOIN pracownicy p ON p.pesel = np.nauczyciel_pesel")
+        cur.execute("SELECT np.przedmiot_nazwa, p.pesel, p.nazwisko || ' ' || p.imie "
+                    "FROM nauczyciele_przedmioty np JOIN pracownicy p ON p.pesel = np.nauczyciel_pesel")
         return [[el[0], el[1], el[2], f"{el[0]} <{el[2]} ({el[1]})>"] for el in cur.fetchall()]
 
     def __get_list_klasy(self):
@@ -59,14 +53,9 @@ class Sprawdziany(Methods):
         return [[el[0], el[1], f"{el[0]} ({el[1]})"] for el in cur.fetchall()]
 
     def __frame_add(self):
-        for x in self.__window.winfo_children():
-            x.destroy()
-        frame = self._create_frame_edit_or_add(self.__window, "Dodanie sprawdzianu",
-                                               self.__list_labels, None,
-                                               [self.__get_list_klasy_full_name(),
-                                                self.__get_list_string_przedmioty_and_teacher(), str, str],
-                                               self.__add_to_db, "Stwórz sprawdzian", self.show_frame)
-        frame.pack()
+        self._create_add_frame(self.__window, "Dodanie sprawdzianu", "Stwórz sprawdzian", self.__list_labels,
+                               [self.__get_list_klasy_full_name(), self.__get_list_string_przedmioty_and_teacher(),
+                                str, str], self.__add_to_db, self.show_frame).pack()
 
     def __add_to_db(self, list_data: list[str]):
         list_data = self.__data_validation(list_data)
@@ -84,15 +73,10 @@ class Sprawdziany(Methods):
                 self.__db.rollback()
 
     def __frame_edit(self, index: int):
-        for x in self.__window.winfo_children():
-            x.destroy()
-        frame = self._create_frame_edit_or_add(self.__window, "Edycja sprawdzianu",
-                                               self.__list_labels, self.__rows[index],
-                                               [self.__get_list_klasy_full_name(),
-                                                self.__get_list_string_przedmioty_and_teacher(), str, str],
-                                               lambda list_data: self.__edit_row_in_db(list_data, index), "Edytuj sprawdzian",
-                                               self.show_frame)
-        frame.pack()
+        self._create_edit_frame(self.__window, "Edycja sprawdzianu", "Edytuj sprawdzian", self.__list_labels,
+                                self.__rows[index], [self.__get_list_klasy_full_name(),
+                                                     self.__get_list_string_przedmioty_and_teacher(), str, str],
+                                lambda data: self.__edit_row_in_db(data, index), self.show_frame).pack()
 
     def __edit_row_in_db(self, list_data, index):
         list_data = self.__data_validation(list_data)
@@ -109,7 +93,7 @@ class Sprawdziany(Methods):
                 messagebox.showerror("Błąd przy edycji sprawdzianu!", "Niezydentyfikowany błąd")
                 self.__db.rollback()
 
-    def __frame_del_row(self, index: int):
+    def __frame_del(self, index: int):
         decision = messagebox.askquestion("Usuwanie rekordu", f"Czy jesteś pewny że chcesz usunąć sprawdzian?")
         if decision == "yes":
             try:
