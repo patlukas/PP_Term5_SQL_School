8da4e08ba89490f6c26bc720aa6a7138da6c68f7
patlukas <lukaszewski.patryk06@gmail.com>
2023-01-07 18:57:19 +0100

    Change Lekcje: improved validation,code refactor

--- Lekcje.py
+++ Lekcje.py
@@ -15,23 +15,16 @@ class Lekcje(Methods):
         self.__rows = []
 
     def show_frame(self) -> None:
-        self.__db.commit()
         self.__list_zajecia = self.__get_list_zajecia()
         self.__rows, self.__list_id = self.__get_rows_data()
-        for x in self.__window.winfo_children():
-            x.destroy()
-        frame = tk.Frame(master=self.__window)
-        tk.Label(master=frame, text="Lekcje").pack()
-        self._create_table(frame, self.__list_labels, self.__rows, self.__frame_edit, self.__frame_del_row).pack()
-        tk.Button(master=frame, text="Dodaj nową lekcje", command=self.__frame_add).pack()
-        frame.pack()
+        self._create_main_frame(self.__db, self.__window, "Lekcje", "Dowaj nową lekcję", self.__list_labels,
+                                self.__rows, self.__frame_add, self.__frame_edit, self.__frame_del).pack()
 
     def __get_rows_data(self):
         cur = self.__db.cursor()
         cur.execute("SELECT * FROM lekcje")
-        rows_read = cur.fetchall()
         rows, list_id = [], []
-        for row in rows_read:
+        for row in cur.fetchall():
             klasa_and_zajecia = ""
             for el in self.__list_zajecia:
                 if row[1] == el[0]:
@@ -44,18 +37,14 @@ class Lekcje(Methods):
     def __get_list_zajecia(self):
         cur = self.__db.cursor()
         cur.execute("SELECT id, przedmioty_nazwa, klasy_nazwa, klasy_rocznik, dzień_zajęc, godzina_zajęć FROM zajecia")
-        return [[el[0], el[4], f"{el[2]} {el[3]} ({el[4]} | {el[4]} {el[5]})"] for el in cur.fetchall()]
+        return [[el[0], el[4], f"{el[2]} {el[3]} ({el[1]} | {el[4]} {el[5]})"] for el in cur.fetchall()]
 
     def __frame_add(self):
-        for x in self.__window.winfo_children():
-            x.destroy()
-        frame = self._create_frame_edit_or_add(self.__window, "Dodanie lekcji", self.__list_labels, None,
-                                               [self.__get_list_zajecia_title(), str, str], self.__add_to_db, "Stwórz",
-                                               self.show_frame)
-        frame.pack()
+        self._create_add_frame(self.__window, "Dodawanie lekcji", "Dodaj lekcję", self.__list_labels,
+                               [self.__get_list_zajecia_title(), str, str], self.__add_to_db, self.show_frame).pack()
 
     def __add_to_db(self, list_data: list[str]):
-        list_data = self.__data_validation(list_data)
+        list_data = self.__data_validation(list_data, -1)
         if list_data is not False:
             try:
                 zajecia_id = self.__get_zajecia_id_by_title(list_data[0])
@@ -68,16 +57,12 @@ class Lekcje(Methods):
                 self.__db.rollback()
 
     def __frame_edit(self, index: int):
-        for x in self.__window.winfo_children():
-            x.destroy()
-        frame = self._create_frame_edit_or_add(self.__window, "Edycja lekcji", self.__list_labels, self.__rows[index],
-                                               [self.__get_list_zajecia_title(), str, str],
-                                               lambda list_data: self.__edit_row_in_db(list_data, index), "Edytuj lekcje",
-                                               self.show_frame)
-        frame.pack()
+        self._create_edit_frame(self.__window, "Edycja lekcji", "Edytuj lekcję", self.__list_labels, self.__rows[index],
+                                [self.__get_list_zajecia_title(), str, str],
+                                lambda data: self.__edit_row_in_db(data, index), self.show_frame).pack()
 
     def __edit_row_in_db(self, list_data, index):
-        list_data = self.__data_validation(list_data)
+        list_data = self.__data_validation(list_data, self.__list_id[index])
         if list_data is not False:
             try:
                 zajecia_id = self.__get_zajecia_id_by_title(list_data[0])
@@ -89,7 +74,7 @@ class Lekcje(Methods):
                 messagebox.showerror("Błąd przy edycji lekcji!", "Niezydentyfikowany błąd")
                 self.__db.rollback()
 
-    def __frame_del_row(self, index: int):
+    def __frame_del(self, index: int):
         decision = messagebox.askquestion("Usuwanie rekordu", f"Czy jesteś pewny że chcesz usunąć lekcje?")
         if decision == "yes":
             try:
@@ -100,7 +85,7 @@ class Lekcje(Methods):
                 messagebox.showerror("Błąd przy usuwaniu rekordu!", f"Usunięcie lekcji się niepowiodło")
                 self.__db.rollback()
 
-    def __data_validation(self, list_data):
+    def __data_validation(self, list_data, lekcje_id):
         if not (
             self.check_value_from_list(list_data[0], self.__get_list_zajecia_title(), "Klasa (Zajęcia)") and
             self.check_date(list_data[1], "Data lekcji") and
@@ -108,15 +93,25 @@ class Lekcje(Methods):
             self.__check_date_day_of_week(list_data[1], self.__get_zajecia_day_of_week_by_title(list_data[0]))
         ):
             return False
+        zajecia_id = self.__get_zajecia_id_by_title(list_data[0])
+        check_data = [
+            [
+                "SELECT * FROM lekcje WHERE zajęcia_id=? AND data_zajęć=? AND id != ?",
+                [zajecia_id, list_data[1], lekcje_id],
+                "Już istnieje lekcja z tych zajęć w tej dacie"
+            ]
+        ]
+        if not self.check_delete_is_possible(self.__db, check_data):
+            return False
         return list_data
 
     def __get_list_zajecia_title(self):
         return [r[2] for r in self.__list_zajecia]
 
     def __get_zajecia_id_by_title(self, title):
-        for id, _, name in self.__list_zajecia:
+        for id_zajec, _, name in self.__list_zajecia:
             if title == name:
-                return id
+                return id_zajec
         raise Exception("Brak id zajeć")
 
     def __get_zajecia_day_of_week_by_title(self, title):
@@ -145,6 +140,3 @@ class Lekcje(Methods):
                                  f"Dzień tygodnia w wybranej dacie to {list_day_of_week[nr_day]}, "
                                  f"a zajęcia odbywają się w {day_of_week}")
         return False
-
-
-
